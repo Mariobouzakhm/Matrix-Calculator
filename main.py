68b9d0bd018e2f5d6e18fa1c0e9a7235d3d68271
Mariobouzakhm <mariobouzakhm02@gmail.com>
2020-06-26 19:56:54 +0300

    Added Matrix Input/Addition

---
+++ main.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Author: Mario Bouzakhm
+Date: 06-2020
+
+Matrix Calculator Interface
+"""
+
+
+def matrixinput():
+    dimensions = [int(x) for x in input("Enter the dimension of the matrix: ").strip().split(" ")]
+    A = []
+    for i in range(dimensions[0]):
+        numbers = [int(x) for x in input("Enter the numbers of line " + str(i + 1) + ": ").split(" ")]
+        while len(numbers) != dimensions[1]:
+            print("Invalid Numbers.")
+            numbers = [int(x) for x in input("Enter the numbers of line " + str(i + 1) + ": ").split(" ")]
+
+        A.append(numbers)
+
+    return A
+
+
+def displaymatrix(matrix):
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            print(str(matrix[i][j])+ " ", end='')
+        print("\n", end='')
+
+
+def additionmatrix(A, B):
+    dimensions1 = [len(A), len(A[0])]
+    dimensions2 = [len(B), len(B[0])]
+
+    if dimensions1[0] != dimensions2[0] or dimensions1[1] != dimensions2[1]:
+        print("ERROR")
+    else:
+        resultMatrix = []
+
+        for i in range(len(A)):
+            resultingLine = []
+            for j in range(len(A[i])):
+                sum = A[i][j] + B[i][j]
+                resultingLine.append(sum)
+
+            resultMatrix.append(resultingLine)
+
+        return resultMatrix
+
+
+A = matrixinput()
+B = matrixinput()
+
+displaymatrix(additionmatrix(A, B))
+
+
+
+
+
