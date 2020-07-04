429c3eae7534f581f01282870bbd559b07f2c58b
Mariobouzakhm <mariobouzakhm02@gmail.com>
2020-07-04 11:43:18 +0300

    Added Matrices Multiplication

--- main.py
+++ main.py
@@ -9,14 +9,22 @@ Matrix Calculator Interface
 """
 
 
-def matrixinput():
-    dimensions = [int(x) for x in input("Enter the dimension of the matrix: ").strip().split(" ")]
+def printmenu():
+    print('1. Add matrices')
+    print('2. Multiply matrix by a constant')
+    print('3. Multiply matrices')
+    print('0. Exit')
+
+
+def matrixinput(order='', func=float):
+    dimensions = [int(x) for x in input("Enter size of " + order + " matrix: ").strip().split(" ")]
     A = []
+    print('Enter ' + order + ' matrix: ')
     for i in range(dimensions[0]):
-        numbers = [int(x) for x in input("Enter the numbers of line " + str(i + 1) + ": ").split(" ")]
+        numbers = [func(x) for x in input().split(" ")]
         while len(numbers) != dimensions[1]:
             print("Invalid Numbers.")
-            numbers = [int(x) for x in input("Enter the numbers of line " + str(i + 1) + ": ").split(" ")]
+            numbers = [func(x) for x in input().split(" ")]
 
         A.append(numbers)
 
@@ -24,6 +32,7 @@ def matrixinput():
 
 
 def displaymatrix(matrix):
+    print('The result is: ')
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
             print(str(matrix[i][j])+ " ", end='')
@@ -35,7 +44,7 @@ def additionmatrix(A, B):
     dimensions2 = [len(B), len(B[0])]
 
     if dimensions1[0] != dimensions2[0] or dimensions1[1] != dimensions2[1]:
-        print("ERROR")
+        print("This operation cannot be performed")
     else:
         resultMatrix = []
 
@@ -64,10 +73,89 @@ def matrixconstantmult(A, constant):
     return resultMatrix
 
 
-A = matrixinput()
-constant = int(input("Enter the constant of multiplication: "))
+def mattricesmult(A, B):
+    dimensions1 = [len(A), len(A[0])]
+    dimensions2 = [len(B), len(B[0])]
 
-displaymatrix(matrixconstantmult(A, constant))
+    if dimensions1[1] != dimensions2[0]:
+        print("This operation cannot be performed.")
+
+    else:
+        result = []
+
+        for i in range(dimensions1[0]):
+            resultLine = []
+
+            list1 = A[i]
+            for j in range(dimensions2[1]):
+                list2 = getverticallist(B, j)
+
+                sum = dotproduct(list1, list2)
+
+                resultLine.append(sum)
+
+
+            result.append(resultLine)
+
+        return result
+
+
+def dotproduct(list1, list2):
+    if len(list1) != len(list2):
+        return 'ERROR'
+    else:
+        sum = 0
+
+        for i in range(len(list1)):
+            sum += (list1[i] * list2[i])
+
+        return sum
+
+
+def getverticallist(matrix, colnum):
+    if colnum > len(matrix):
+        return 'ERROR'
+
+    col = []
+    for i in range(len(matrix)):
+        col.append(matrix[i][colnum])
+
+    return col
+
+
+def main():
+    while True:
+        printmenu()
+
+        cmd = input('Your choice: ')
+
+        if cmd == '1':
+            A = matrixinput(order='first')
+            B = matrixinput(order='second')
+
+            C = additionmatrix(A, B)
+            displaymatrix(C)
+
+        elif cmd == '2':
+            A = matrixinput()
+            constant = float(input('Enter constant: '))
+
+            B = matrixconstantmult(A, constant)
+            displaymatrix(B)
+
+        elif cmd == '3':
+            A = matrixinput(order='first')
+            B = matrixinput(order='second')
+
+            C = mattricesmult(A, B)
+            displaymatrix(C)
+
+        elif cmd == '0':
+            break
+
+        print("")
+
+main()
 
 
 
